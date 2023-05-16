82b6793a92a5aefe65f597dcbb5b3a0a4d4bc405
gamwed <14orbit@gmail.com>
2023-05-16 13:39:13 +0300

    WORK

---
+++ main.py
@@ -0,0 +1,31 @@
+# t = int(input("temp: "))
+# if t < 0:
+#     print("very cold!")
+# else:
+#     print("shrko!!")
+# print('have a nice day')
+
+# point = int(input('бал: '))
+# if point >= 90:
+#     print("A")
+# elif point >= 85:
+#     print("B")
+# elif point >= 75:
+#     print("C")
+# elif point >= 70:
+#     print("D")
+# elif point >= 60:
+#     print("E")
+# else:
+#     print("F")
+#
+# х = 10
+#
+# if x == 10:
+#     x = 0
+
+# x = float(input("cord x "))
+# y = float(input("cord y "))
+# print(y<=1 and y>=abs(x))
+
+X = []
